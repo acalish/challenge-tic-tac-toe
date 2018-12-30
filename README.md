59ef62dac7b426e8d3dc7adfc252789305e68a96
acalish <acalish@gmail.com>
2018-12-30 13:31:45 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,6 @@
+## Tic-tac-toe take home challenge
+
+Setup
+1. Fork and clone this repository
+2. npm install
+3. grunt serve
---
+++ index.js
@@ -0,0 +1,8 @@
+'use strict'
+import 'bootstrap'
+
+require('babel-polyfill')
+
+require('./assets/scripts/app.js')
+
+require('./assets/styles/index.scss')
