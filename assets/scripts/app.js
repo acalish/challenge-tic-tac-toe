ede01cd9b6de4568e26250ca714edc5090c733a8
acalish <acalish@gmail.com>
2018-12-30 15:50:44 -0500

    Only alternate users on valid turn
    - when a cell is clicked, only add user symbol and alternate user if
    that spot has not been clicked before
    - else, remain on current user until valid selection

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,18 +4,22 @@ $(() => {
   // initialize game with empty array
   const gameArray = ['', '', '', '', '', '', '', '', '']
   let currentUser = 'X'
+
   $('.cell').on('click', function (event) {
     console.log('clicked', event.target.id)
-    // add current user into array
-    gameArray[event.target.id] = currentUser
-    console.log(gameArray)
-    // alternate users
-    if (currentUser === 'X') {
-      $(event.target).text('X')
-      currentUser = 'O'
-    } else {
-      $(event.target).text('O')
-      currentUser = 'X'
+    // check if cell has been clicked before
+    if (gameArray[event.target.id] === '') {
+      // add current user into array
+      gameArray[event.target.id] = currentUser
+      console.log(gameArray)
+      // alternate users
+      if (currentUser === 'X') {
+        $(event.target).text('X')
+        currentUser = 'O'
+      } else {
+        $(event.target).text('O')
+        currentUser = 'X'
+      }
     }
   })
 })
