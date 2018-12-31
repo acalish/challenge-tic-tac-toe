4b5b852e3497c093886fab09f687b26d02378b18
acalish <acalish@gmail.com>
2018-12-31 12:09:40 -0500

    Refactor, rename
    - rename takeTurn function to updateGame function to be more semantic
    - move functionality to visually alter gameboard from alternateUser
    function to updateGame function

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,26 +1,28 @@
 'use strict'
 
-// initialize game with empty array
 const gameArray = ['', '', '', '', '', '', '', '', '']
 let currentUser = 'X'
 
 const alternateUser = function () {
   if (currentUser === 'X') {
-    $(event.target).text('X')
     currentUser = 'O'
   } else {
-    $(event.target).text('O')
     currentUser = 'X'
   }
 }
 
-$('.cell').on('click', function (event) {
-  // check if cell has been clicked before
-  if (gameArray[event.target.id] === '') {
-    // add current user into array
-    gameArray[event.target.id] = currentUser
-    console.log(gameArray)
-    // alternate users
-    alternateUser()
-  }
-})
+const updateGame = function () {
+  $('.cell').on('click', function (event) {
+    if (gameArray[event.target.id] === '') {
+      gameArray[event.target.id] = currentUser
+      $(event.target).text(currentUser)
+      alternateUser()
+    }
+  })
+}
+
+const playGame = function () {
+  updateGame()
+}
+
+playGame()
