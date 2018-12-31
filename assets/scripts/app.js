4fa0f5a56999b3adec91bfb71fee8d529d0ea524
acalish <acalish@gmail.com>
2018-12-31 11:45:50 -0500

    Refactor, update styling
    - add class container to game board to center it
    - center text
    - start to refactor app.js into smaller functions

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,25 +1,26 @@
 'use strict'
 
-$(() => {
-  // initialize game with empty array
-  const gameArray = ['', '', '', '', '', '', '', '', '']
-  let currentUser = 'X'
+// initialize game with empty array
+const gameArray = ['', '', '', '', '', '', '', '', '']
+let currentUser = 'X'
 
-  $('.cell').on('click', function (event) {
-    console.log('clicked', event.target.id)
-    // check if cell has been clicked before
-    if (gameArray[event.target.id] === '') {
-      // add current user into array
-      gameArray[event.target.id] = currentUser
-      console.log(gameArray)
-      // alternate users
-      if (currentUser === 'X') {
-        $(event.target).text('X')
-        currentUser = 'O'
-      } else {
-        $(event.target).text('O')
-        currentUser = 'X'
-      }
-    }
-  })
+const alternateUser = function () {
+  if (currentUser === 'X') {
+    $(event.target).text('X')
+    currentUser = 'O'
+  } else {
+    $(event.target).text('O')
+    currentUser = 'X'
+  }
+}
+
+$('.cell').on('click', function (event) {
+  // check if cell has been clicked before
+  if (gameArray[event.target.id] === '') {
+    // add current user into array
+    gameArray[event.target.id] = currentUser
+    console.log(gameArray)
+    // alternate users
+    alternateUser()
+  }
 })
