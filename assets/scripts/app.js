cf39dcdc96c15255b702b38f7b8c09ac65b75c87
acalish <acalish@gmail.com>
2019-01-04 13:03:13 -0500

    Add check for winning combos

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,7 +1,36 @@
 'use strict'
 
-const gameArray = ['', '', '', '', '', '', '', '', '']
+const gA = ['', '', '', '', '', '', '', '', '']
 let currentUser = 'X'
+
+// check winning scenarios - 8 winning combinations
+// look at gameArray, see who won
+
+const winningCombos = [[0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
+
+const combination = function (array) {
+  if (gA[array[0]] === gA[array[1]] && gA[array[1]] === gA[array[2]] && gA[array[1]] !== '') {
+    return true
+  } else return false
+}
+
+const haveWinner = function (winningCombos) {
+  let result = false
+  for (let i = 0; i < winningCombos.length; i++) {
+    if (combination(winningCombos[i])) {
+      result = true
+    }
+  }
+  return result
+}
 
 const alternateUser = function () {
   if (currentUser === 'X') {
@@ -14,11 +43,12 @@ const alternateUser = function () {
 
 const updateGame = function () {
   $('.cell').on('click', function (event) {
-    if (gameArray[event.target.id] === '') {
-      gameArray[event.target.id] = currentUser
+    if (gA[event.target.id] === '') {
+      gA[event.target.id] = currentUser
       $(event.target).text(currentUser)
       currentUser = alternateUser(currentUser)
     }
+    haveWinner(winningCombos)
   })
 }
 
